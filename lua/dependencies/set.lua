9ec99282988575cd22f9612d37bef96dc0390b81
kyle mathews <kmathews@gryphon.ai>
2025-11-30 03:08:06 -0500

    forcing tabs

--- lua/dependencies/set.lua
+++ lua/dependencies/set.lua
@@ -1,6 +1,21 @@
 vim.opt.nu = true
 vim.opt.wrap = true
-
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function(args)
+    -- Exception: Force spaces for YAML files only
+    if args.match == "yaml" then
+      vim.opt_local.expandtab = true
+      vim.opt_local.tabstop = 2
+      vim.opt_local.shiftwidth = 2
+    else
+      -- Everyone else gets tabs
+      vim.opt_local.expandtab = false
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+    end
+  end,
+})
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
